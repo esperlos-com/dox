1490739a2450149c19f484a8a7fa10c60bf5c0d9
sajjad.ws <sajjad.ws@gmail.com>
2023-02-28 23:28:31 +0330

    fix: error 419

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('login', ['as' => 'login', 'uses' => \App\Http\Livewire\Panel\Pages\Login::class]);
+Route::get('admin-login', ['as' => 'login', 'uses' => \App\Http\Livewire\Panel\Pages\Login::class]);
 
 
 
